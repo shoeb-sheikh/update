9e4f9cf32316ee42040cae14d9533f5db6b6c698
shoeb_sheikh <shoebsheikh2542@gmil.com>
2020-05-13 08:55:23 +0530

    update

---
+++ update.sh
@@ -0,0 +1,60 @@
+
+#!/bin/bash
+apt install figlet
+clear
+figlet  Start
+figlet Updating
+echo ""
+echo ""
+echo ""
+echo  "_______________________________________"
+echo  " turn on your internet connection first"
+echo  "_______________________________________"
+echo "......................................."
+echo "....................................."
+
+echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo   " its taking upto 1 to 2 minutes"
+echo   " take a long breath & relax for "
+echo   "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+echo "......................................."
+echo "....................................."
+
+apt update && apt upgrade
+pkg install sl
+pkg install 
+pkg update && pkg upgrade
+apt install python && apt install python2 
+apt install ruby 
+apt install git
+pkg install fish 
+apt install nano
+clear
+sl 
+clear
+echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo   " All right all pakages are update and upgrade   "
+echo  "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo ""
+echo ""
+echo ""
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo " This script update and upgrade your all pakages  "
+echo ""
+echo  "& termux application"
+echo "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+
+echo""
+echo""
+echo "     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+echo "      This script writed by Shoeb Sheikh "
+echo "        please like and subscribe (FOCS)"
+echo "            Future of Computer Science"
+echo "                You Tube  Channel "
+echo "     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ "
+echo ""
+echo "           Thank You for use script    "
+figlet lets Start
+
